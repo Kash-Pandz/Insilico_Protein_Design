9cccaef8299a41ef8538d9dc0ba19bc60c7ef469
Kash-Pandz <86837869+Kash-Pandz@users.noreply.github.com>
2025-05-09 11:29:08 +0100

    Update colabfold.sh

--- colabfold.sh
+++ colabfold.sh
@@ -4,4 +4,7 @@ OUTPUT_DIR=gen_seq_preds/
 FASTA_DIR=gen_seq_fastas/
 mkdir -p $OUTPUT_DIR
 
-colabfold_batch $FASTA_DIR $OUTPUT_DIR --num-models 1 --model-type alphafold2_multimer_v3
+colabfold_batch $FASTA_DIR $OUTPUT_DIR \
+  --num-models 1 \
+  --model-type alphafold2_multimer_v3 \
+  --num-recycle 3
