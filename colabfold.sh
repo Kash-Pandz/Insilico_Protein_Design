ec1a93f8ed239f750c870fa69a95ec29d7e88622
Akash Pandya <86837869+Kash-Pandz@users.noreply.github.com>
2025-09-22 11:02:17 +0100

    Update colabfold.sh

--- colabfold.sh
+++ colabfold.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+module unload python
+module load cuda/12.4.0
+module load anaconda
 
+# Activate conda environment
+conda activate colabfold-conda
+
+# Input fasta directory and output directory where predicted structures will be stored
 OUTPUT_DIR=gen_seq_preds/
 FASTA_DIR=gen_seq_fastas/
 mkdir -p $OUTPUT_DIR
 
+# Run command for colabfold_batch with input fasta 
 colabfold_batch $FASTA_DIR $OUTPUT_DIR \
   --num-models 1 \
   --model-type alphafold2_multimer_v3 \
