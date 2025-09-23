6050822bc6355007b1beda9b59438a44574bd046
Akash Pandya <86837869+Kash-Pandz@users.noreply.github.com>
2025-09-23 15:52:25 +0100

    Added contig map function for RFDiffusion

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os
+import MDAnalysis as mda
 
-def get_res(res_input):
+def get_res_string(res_input):
     """ 
     Returns a space-separated string of residues from a string or txt file.
 
@@ -26,6 +27,50 @@ def get_res(res_input):
             expanded.append(part)
           
     return ' ' .join(expanded)
-   
 
-   
+
+def get_contig_map(pdb_file: str, fixed_residues: list[int], chain_id="A": str) -> str:
+    """
+    Creates a contig map for RFDiffusion from PDB file.
+
+    Args:
+        pdb_file: Path to input PDB file.
+        res_sel: Residue IDs (PDB numbering).
+        chain_id: Chain ID to use for contig string.
+
+    Returns:
+        Contig map
+
+    """
+    # Load PDB file in MDAnalyis universe object
+    universe = mda.Universe(pdb_file)
+
+    # Extract sorted residue IDs
+    resids = sorted[res.resid for res in universe.residues])
+
+    contig_map = []
+
+    # Preceding block before first selected residue
+    first_res = resid[0]
+    first_fixed = fixed_residues[0]
+    if first_fixed > first_res:
+        seg_len = first_fixed - first_res
+        contig_map.append(f"{seg_len}-{seg_len}")
+
+    # In-between fixed residue and gaps between them
+    for i, fixed_res in enumerate(fixed_residues):
+        if i > 0:
+            prev_res = fixed_residues[i-1]
+            gap_len = fixed_res - prev_res - 1
+            if gap_len > 0:
+                contig_map.append(f"{gap_len}-{gap_len}")
+        contig_map.append(f"{chain_id}(fixed_res}-{fixed_res}")
+
+   # Block after last fixed residue
+   last_fixed = fixed_residues[-1]
+   last_res = resids[-1]
+   if last_res > last_fixed:
+       seg_len = last_res - last_fixed
+       contig_map.append(f"{seg_len}-{seg_len}")
+
+   return "[" + "/".join(contig_map) + "]"
